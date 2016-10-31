3fcb5817061adb72fb9fabb3d3211d35a43105a9
Kenan Yildirim <kenan@kenany.me>
2016-10-31 19:11:40 -0400

    deps: remove lodash.isarray
    Closes https://github.com/KenanY/chemical-symbols/issues/3

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
 var symbols = require('../');
 var test = require('tape');
-var isArray = require('lodash.isarray');
 
 test('exports an array', function(t) {
   t.plan(1);
-  t.ok(isArray(symbols));
+  t.ok(Array.isArray(symbols));
 });
 
 test('has all 118 elements', function(t) {
